8bdcab64e05d016c4caff885dc7fb9d981182022
lidia adel <lidiaadel77@gmail.com>
2025-07-12 22:00:49 +0300

    Created azure function api

--- frontend/main.js
+++ frontend/main.js
@@ -2,10 +2,10 @@ window.addEventListener('DOMContentLoaded', (event) => {
     getVisitCount();
 });
 
-const functionApi = '';
+const functionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 const getVisitCount = () => {
-    let count =30
+    let count =30;
     fetch(functionApi)
         .then(response => {
             return response.json()})
